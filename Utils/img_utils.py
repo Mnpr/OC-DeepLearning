5dd42d676a4136e4ac737c9660dded71ab582907
Mnpr <acharya.sudesh366@gmail.com>
2021-01-25 04:09:02 +0100

    utils remove loader + mod other utils

--- Utils/img_utils.py
+++ Utils/img_utils.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from keras import backend as K
 from keras.preprocessing import image
+from sklearn.metrics import roc_auc_score, roc_curve
 from tensorflow.compat.v1.logging import INFO, set_verbosity
 
 random.seed(a=None, version=2)
@@ -81,3 +82,29 @@ def compute_gradcam(model, img, image_dir, df, labels, selected_labels,
                        cmap='gray')
             plt.imshow(gradcam, cmap='jet', alpha=min(0.5, predictions[0][i]))
             j += 1
+
+
+def get_roc_curve(labels, predicted_vals, generator):
+    auc_roc_vals = []
+    for i in range(len(labels)):
+        try:
+            gt = generator.labels[:, i]
+            pred = predicted_vals[:, i]
+            auc_roc = roc_auc_score(gt, pred)
+            auc_roc_vals.append(auc_roc)
+            fpr_rf, tpr_rf, _ = roc_curve(gt, pred)
+            plt.figure(1, figsize=(10, 10))
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.plot(fpr_rf, tpr_rf,
+                     label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
+            plt.xlabel('False positive rate')
+            plt.ylabel('True positive rate')
+            plt.title('ROC curve')
+            plt.legend(loc='best')
+        except:
+            print(
+                f"Error in generating ROC curve for {labels[i]}. "
+                f"Dataset lacks enough examples."
+            )
+    plt.show()
+    return auc_roc_vals
