9f413ca01a2583ad790cc4d7c09e756fb0b596ee
Mnpr <acharya.sudesh366@gmail.com>
2021-01-25 20:14:24 +0100

    loss and img utils update]

--- Utils/img_utils.py
+++ Utils/img_utils.py
@@ -13,7 +13,33 @@ random.seed(a=None, version=2)
 set_verbosity(INFO)
 
 
-def get_mean_std_per_batch(image_path, df, H=320, W=320):
+def get_roc_curve(labels, predicted_vals, generator):
+    auc_roc_vals = []
+    for i in range(len(labels)):
+        try:
+            gt = generator.labels[:, i]
+            pred = predicted_vals[:, i]
+            auc_roc = roc_auc_score(gt, pred)
+            auc_roc_vals.append(auc_roc)
+            fpr_rf, tpr_rf, _ = roc_curve(gt, pred)
+            plt.figure(1, figsize=(10, 10))
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.plot(fpr_rf, tpr_rf,
+                     label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
+            plt.xlabel('False positive rate')
+            plt.ylabel('True positive rate')
+            plt.title('ROC curve')
+            plt.legend(loc='best')
+        except:
+            print(
+                f"Error in generating ROC curve for {labels[i]}. "
+                f"Dataset lacks enough examples."
+            )
+    plt.show()
+    return auc_roc_vals
+
+
+def get_mean_std_per_batch(image_path, df, H=256, W=256):
     sample_data = []
     for idx, img in enumerate(df.sample(100)["Image"].values):
         # path = image_dir + img
@@ -25,7 +51,7 @@ def get_mean_std_per_batch(image_path, df, H=320, W=320):
     return mean, std
 
 
-def load_image(img, image_dir, df, preprocess=True, H=320, W=320):
+def load_image(img, image_dir, df, preprocess=True, H=256, W=256):
     """Load and preprocess image."""
     img_path = image_dir + img
     mean, std = get_mean_std_per_batch(img_path, df, H=H, W=W)
@@ -37,7 +63,7 @@ def load_image(img, image_dir, df, preprocess=True, H=320, W=320):
     return x
 
 
-def grad_cam(input_model, image, cls, layer_name, H=320, W=320):
+def grad_cam(input_model, image, cls, layer_name, H=256, W=256):
     """GradCAM method for visualizing input saliency."""
     y_c = input_model.output[0, cls]
     conv_output = input_model.get_layer(layer_name).output
@@ -82,29 +108,3 @@ def compute_gradcam(model, img, image_dir, df, labels, selected_labels,
                        cmap='gray')
             plt.imshow(gradcam, cmap='jet', alpha=min(0.5, predictions[0][i]))
             j += 1
-
-
-def get_roc_curve(labels, predicted_vals, generator):
-    auc_roc_vals = []
-    for i in range(len(labels)):
-        try:
-            gt = generator.labels[:, i]
-            pred = predicted_vals[:, i]
-            auc_roc = roc_auc_score(gt, pred)
-            auc_roc_vals.append(auc_roc)
-            fpr_rf, tpr_rf, _ = roc_curve(gt, pred)
-            plt.figure(1, figsize=(10, 10))
-            plt.plot([0, 1], [0, 1], 'k--')
-            plt.plot(fpr_rf, tpr_rf,
-                     label=labels[i] + " (" + str(round(auc_roc, 3)) + ")")
-            plt.xlabel('False positive rate')
-            plt.ylabel('True positive rate')
-            plt.title('ROC curve')
-            plt.legend(loc='best')
-        except:
-            print(
-                f"Error in generating ROC curve for {labels[i]}. "
-                f"Dataset lacks enough examples."
-            )
-    plt.show()
-    return auc_roc_vals
